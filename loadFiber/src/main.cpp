0abb481dcf6812bd1ec770de9946ab14fadcae63
Patrick Bellavance <patrickbellavance@hotmail.com>
2013-10-10 15:48:03 -0400

    change tab for space in main.cpp

--- loadFiber/src/main.cpp
+++ loadFiber/src/main.cpp
@@ -2,9 +2,9 @@
 
 int main( int argc, const char* argv[] )
 {
-	std::string fileName = "fibers.fib";
-	//load file fiber
-	fiber myFiber;
+    std::string fileName = "fibers.fib";
+    //load file fiber
+    fiber myFiber;
 
-	myFiber.load(fileName);
+    myFiber.load(fileName);
 }
